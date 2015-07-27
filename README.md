9d2bc134955a5d0f84a3cd9192c398319c76bc43
Jon Sime <jonsime@gmail.com>
2015-07-27 20:17:12 +0000

    skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# Monitor::MetricsAPI
+
+Statistics collection and metrics reporting API for Perl applications
+
---
+++ lib/Monitor/MetricsAPI.pm
@@ -0,0 +1,7 @@
+package Monitor::MetricsAPI;
+# ABSTRACT: Statistics collection and metrics reporting for Perl applications.
+
+use strict;
+use warnings;
+
+1;
